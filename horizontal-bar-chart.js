b46e935ac710dc748ea5945604aacc4041f56ca9
chaochin <angel253698@gmail.com>
2022-11-27 23:23:30 -0500

    bar chart word cloud

--- horizontal-bar-chart.js
+++ horizontal-bar-chart.js
@@ -1,7 +1,12 @@
-// set the dimensions and margins of the graph
+// set the dimensions and margins of the bar chart
 const margin = { top: 5, right: 5, bottom: 30, left: 150 },
   width = 400 - margin.left - margin.right,
   height = 600 - margin.top - margin.bottom;
+
+// set the dimensions and margins of the word cloud
+var margin_wordcloud = { top: 10, right: 10, bottom: 10, left: 10 },
+  width_wordcloud = 350 - margin_wordcloud.left - margin_wordcloud.right,
+  height_wordcloud = 350 - margin_wordcloud.top - margin_wordcloud.bottom;
 
 // append the svg object to the body of the page
 const svg = d3
@@ -16,7 +21,6 @@ const svg = d3
 d3.csv(
   "https://raw.githubusercontent.com/a253698/INLS641-Wellbeing-Dashboard/main/ml.csv"
 ).then(function (data) {
-  //  console.log(data);
   // Add X axis
   const x = d3.scaleLinear().domain([0, 0.1]).range([0, width]);
   svg
@@ -52,5 +56,74 @@ d3.csv(
     .attr("y", (d) => y(d.Feature))
     .attr("width", (d) => x(d.FeatureImportance))
     .attr("height", y.bandwidth())
-    .attr("fill", "#69b3a2");
+    .attr("fill", "#69b3a2")
+    .on("mouseover", function (event, d) {
+      //  console.log("In" + d.WordCloud);
+      update(d.WordCloud);
+      $("#my_dataviz").css("visibility", "visible");
+    })
+    .on("mousemove", function (event, d) {})
+    .on("mouseleave", function (event, d) {});
+
+  // append the svg object to the body of the page
+  var svg_wordcloud = d3
+    .select("#my_dataviz")
+    .append("svg")
+    .attr(
+      "width",
+      width_wordcloud + margin_wordcloud.left + margin_wordcloud.right
+    )
+    .attr(
+      "height",
+      height_wordcloud + margin_wordcloud.top + margin_wordcloud.bottom
+    )
+    .append("g")
+    .attr(
+      "transform",
+      "translate(" + margin_wordcloud.left + "," + margin_wordcloud.top + ")"
+    );
+
+  function draw(data) {
+    svg_wordcloud
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + layout.size()[0] / 2 + "," + layout.size()[0] / 2 + ")"
+      )
+      .selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .style("font-size", 20)
+      .style("fill", "#69b3a2")
+      .attr("text-anchor", "middle")
+      .style("font-family", "Impact")
+      .attr("transform", function (d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function (d) {
+        return d.text;
+      });
+  }
+
+  function update(data) {
+    svg_wordcloud.text("");
+
+    var update_data = data.split(" ").map(function (d) {
+      return { text: d };
+    });
+
+    var layout = d3.layout
+      .cloud()
+      .size([width, height])
+      .words(update_data)
+      .padding(5) //space between words
+      .rotate(0) // rotation angle in degrees
+      .fontSize(20) // font size of words
+      .on("end", draw);
+
+    layout.start();
+  }
+
+  var layout = d3.layout.cloud();
 });
